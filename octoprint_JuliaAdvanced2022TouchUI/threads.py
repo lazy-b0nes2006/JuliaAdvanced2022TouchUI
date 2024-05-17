4a629b67b3e3439400428f261c65c81af1693169
Arnav <arnavtogi@gmail.com>
2024-05-17 16:09:10 +0530

    Fixed error

--- octoprint_JuliaAdvanced2022TouchUI/threads.py
+++ octoprint_JuliaAdvanced2022TouchUI/threads.py
@@ -1,21 +1,26 @@
 from PyQt5 import QtCore
 from octoprintAPI import octoprintAPI
 import subprocess
-from config import ip, apiKey
+from config import ip, apiKey, Development
 import time
 import os
 from network_utils import getIP
 
 class ThreadSanityCheck(QtCore.QThread):
-    def __init__(self, logger, virtual=False):
+
+    loaded_signal = QtCore.pyqtSignal()
+    startup_error_signal = QtCore.pyqtSignal()
+
+    def __init__(self, logger = None, virtual=False):
         super(ThreadSanityCheck, self).__init__()
         self.MKSPort = None
         self.virtual = virtual
-        self._logger = logger
+        if not Development:
+            self._logger = logger
 
     def run(self):
         global octopiclient
-        shutdown_flag = False
+        self.shutdown_flag = False
         # get the first value of t1 (runtime check)
         uptime = 0
         # keep trying untill octoprint connects
@@ -23,39 +28,34 @@ class ThreadSanityCheck(QtCore.QThread):
             # Start an object instance of octopiAPI
             try:
                 if (uptime > 30):
-                    shutdown_flag = True
-                    self.emit(QtCore.SIGNAL('STARTUP_ERROR'))
+                    self.shutdown_flag = True
+                    self.startup_error_signal.emit()
                     break
                 octopiclient = octoprintAPI(ip, apiKey)
-                result = subprocess.Popen("dmesg | grep 'ttyUSB'", stdout=subprocess.PIPE, shell=True).communicate()[0]
-                result = result.split('\n')  # each ssid and pass from an item in a list ([ssid pass,ssid paas])
-                result = [s.strip() for s in result]
                 if not self.virtual:
                     result = subprocess.Popen("dmesg | grep 'ttyUSB'", stdout=subprocess.PIPE, shell=True).communicate()[0]
-                    result = result.split('\n')  # each ssid and pass from an item in a list ([ssid pass,ssid paas])
+                    result = result.split(b'\n')  # each ssid and pass from an item in a list ([ssid pass,ssid paas])
+                    print(result)
                     result = [s.strip() for s in result]
                     for line in result:
-                        if 'ch341-uart' in line:
-                            self.MKSPort = line[line.index('ttyUSB'):line.index('ttyUSB') + 7]
-                            print (self.MKSPort)
-                        elif 'FTDI' in line:
-                            self.MKSPort = line[line.index('ttyUSB'):line.index('ttyUSB') + 7]
-                            print (self.MKSPort)
+                        if b'FTDI' in line:
+                            self.MKSPort = line[line.index(b'ttyUSB'):line.index(b'ttyUSB') + 7].decode('utf-8')
+                            print(self.MKSPort)
+                        if b'ch34' in line:
+                            self.MKSPort = line[line.index(b'ttyUSB'):line.index(b'ttyUSB') + 7].decode('utf-8')
+                            print(self.MKSPort)
 
-                if not self.MKSPort:
-                    octopiclient.connectPrinter(port="VIRTUAL", baudrate=115200)
-                else:
-                    octopiclient.connectPrinter(port="/dev/" + self.MKSPort, baudrate=115200)
+                    if not self.MKSPort:
+                        octopiclient.connectPrinter(port="VIRTUAL", baudrate=115200)
+                    else:
+                        octopiclient.connectPrinter(port="/dev/" + self.MKSPort, baudrate=115200)
                 break
             except Exception as e:
                 time.sleep(1)
                 uptime = uptime + 1
-                # print "Not Connected!"
-                print(e.message)
-                self._logger.error("ThreadSanityCheck: " + str(e.message))
-        if not shutdown_flag:
-            self.emit(QtCore.SIGNAL('LOADED'))
-
+                print("Not Connected!")
+        if not self.shutdown_flag:
+            self.loaded_signal.emit()
 
 class ThreadFileUpload(QtCore.QThread):
     def __init__(self, file, prnt=False):
@@ -77,34 +77,39 @@ class ThreadFileUpload(QtCore.QThread):
         else:
             octopiclient.uploadGcode(file=self.file, select=False, prnt=False)
 
-
 class ThreadRestartNetworking(QtCore.QThread):
     WLAN = "wlan0"
     ETH = "eth0"
+    signal = QtCore.pyqtSignal('PyQt_PyObject')
 
-    def __init__(self, interface, signal):
+    def __init__(self, interface):
         super(ThreadRestartNetworking, self).__init__()
         self.interface = interface
-        self.signal = signal
-
     def run(self):
         self.restart_interface()
         attempt = 0
         while attempt < 3:
+            # print(getIP(self.interface))
             if getIP(self.interface):
-                self.emit(QtCore.SIGNAL(self.signal), getIP(self.interface))
+                self.signal.emit(getIP(self.interface))
                 break
             else:
                 attempt += 1
-                time.sleep(1)
+                time.sleep(5)
         if attempt >= 3:
-            self.emit(QtCore.SIGNAL(self.signal), None)
+            self.signal.emit(None)
 
     def restart_interface(self):
         '''
         restars wlan0 wireless interface to use new changes in wpa_supplicant.conf file
         :return:
         '''
-        subprocess.call(["ifdown", "--force", self.interface], shell=False)
-        subprocess.call(["ifup", "--force", self.interface], shell=False)
+        if self.interface == "wlan0":
+            subprocess.call(["wpa_cli","-i",  self.interface, "reconfigure"], shell=False)
+        if self.interface == "eth0":
+            subprocess.call(["ifconfig",  self.interface, "down"], shell=False)
+            time.sleep(1)
+            subprocess.call(["ifconfig", self.interface, "up"], shell=False)
+        # subprocess.call(["ifdown", "--force", self.interface], shell=False)
+        # subprocess.call(["ifup", "--force", self.interface], shell=False)
         time.sleep(5)
